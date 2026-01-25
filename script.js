7fea78b1d413758d8bbbe009f73dfb4e76a45b31
Akuya <Manda.glxa@gmail.com>
2026-01-25 14:00:06 +0300

    feature : Backend firebase

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-analytics.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyB3E2pu2CLs7z5TpmCcComb5_s10CnGxR0",
+  authDomain: "miniprojet-15888.firebaseapp.com",
+  projectId: "miniprojet-15888",
+  storageBucket: "miniprojet-15888.firebasestorage.app",
+  messagingSenderId: "928633813181",
+  appId: "1:928633813181:web:e1061cdb9104f447d05fd4",
+  measurementId: "G-NS0B0408Z8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Fonction pour envoyer un message de test
+async function envoyerTest() {
+  try {
+    const docRef = await addDoc(collection(db, "messages"), {
+      texte: "Salut l'équipe !",
+      date: new Date()
+    });
+    console.log("Message envoyé avec l'ID : ", docRef.id);
+    alert("Connexion réussie ! Le message est dans Firebase.");
+  } catch (e) {
+    console.error("Erreur d'ajout : ", e);
+  }
+}
+
+// Appelle la fonction pour tester tout de suite au chargement
+envoyerTest();
+
 
 function showChallenge(){
 
